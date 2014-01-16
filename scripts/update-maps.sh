42ceca5039d7b3d50457c1f0c75f53dbfb87d962
Samual Lenks <samual@xonotic.org>
2014-01-16 18:00:39 -0500

    Fixes for update-maps.sh script by Cobra_Fast

--- scripts/update-maps.sh
+++ scripts/update-maps.sh
@@ -9,14 +9,15 @@ for local in $XON_MAP_DIR/*; do
 			MAP_PRESENT=1
 		fi
 	done
-	if [ $local != "$XON_MAP_DIR/*" ] && [ $MAP_PRESENT -eq 0 ]; then
-		rm $local
+	if [ $local != "$XON_MAP_DIR/*" ] && [[ $local == *.pk3 ]] && [ $MAP_PRESENT -eq 0 ]; then
+		rm -v $local
 	fi
 done
 
 for remote in $(cat $XON_MAP_LIST_FILE); do
 	MAP=$(basename $remote)
 	if [ ! -f $MAP ]; then
+		echo "Downloading $remote"
 		wget -qO "$XON_MAP_DIR/$MAP" "$remote"
 	fi
 done
