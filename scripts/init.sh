e39096615224d9270257564bfd4df6efabce129c
Samual Lenks <samual@xonotic.org>
2014-01-16 21:11:07 -0500

    Update minor issue with init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,7 @@ XON_PROFILE="bitmissile"
 XON_PASS="example"
 XON_GAMEDIR="$HOME/xonotic/"
 XON_HUBREPO="$HOME/hubservers/"
-XON_COMMON="./all run dedicated +serverconfig +set \_profile \"$XON_PROFILE\" +set rcon_password \"$XON_PASS\""
+XON_COMMON="./all run dedicated +set \_profile \"$XON_PROFILE\" +set rcon_password \"$XON_PASS\" +serverconfig"
 
 XON_MAP_LIST_FILE="../packagelist-eu.txt"
 XON_MAP_DIR="$HOME/xonoticmaps"
