297ccd4b7dd8a8fccf6850d086ffe15dfbeb478b
Samual Lenks <samual@xonotic.org>
2014-01-20 00:15:19 -0500

    Set maxplayers for the servers as well

--- control.sh
+++ control.sh
@@ -38,15 +38,16 @@ function _xon-start-explicit() {
 	#  ${5}: config
 	#  ${6}: irc
 	#  ${7}: _hooked_profile
-	#  ${8}: _hooked_port
-	#  ${9}: _hooked_public
-	#  ${10}: _hooked_type
-	#  ${11}: _hooked_commands
-	#  ${12}: _hooked_desc
-	#  ${13}: _allow_extra_votes
+	#  ${8}: _hooked_players
+	#  ${9}: _hooked_port
+	#  ${10}: _hooked_public
+	#  ${11}: _hooked_type
+	#  ${12}: _hooked_commands
+	#  ${13}: _hooked_desc
+	#  ${14}: _allow_extra_votes
 
 	# check parameters
-	if [ $# -ne 13 ]
+	if [ $# -ne 14 ]
 	then
 		echo "Incorrect parameters for xon-start-explicit!"
 		echo "Input: \"$@\""
@@ -62,13 +63,14 @@ function _xon-start-explicit() {
 	fi
 
 	_hooked_profile="+set _hooked_profile \"${7}\""
-	_hooked_port="+set _hooked_port \"${8}\""
-	_hooked_public="+set _hooked_public \"${9}\""
-	_hooked_type="+set _hooked_type \"${10}\""
+	_hooked_players="+set _hooked_players \"${8}\""
+	_hooked_port="+set _hooked_port \"${9}\""
+	_hooked_public="+set _hooked_public \"${10}\""
+	_hooked_type="+set _hooked_type \"${11}\""
 	_hc="+set _hc_enabled \"${11}\""
-	_hooked_commands="+alias _hooked_commands \"${11}\""
-	_hooked_desc="+set _hooked_desc \"${12}\""
-	_allow_extra_votes="+set _allow_extra_votes \"${13}\""
+	_hooked_commands="+alias _hooked_commands \"${12}\""
+	_hooked_desc="+set _hooked_desc \"${13}\""
+	_allow_extra_votes="+set _allow_extra_votes \"${14}\""
 
 	# now that we're done handling arguments/variables, execute
 	# rcon2irc first in case we need to attach the server screen.
@@ -86,13 +88,13 @@ function _xon-start-explicit() {
 	# note that it is a single command, escaped into multiple lines!
 	echo "cd $XON_GAMEDIR"
 	cd "$XON_GAMEDIR"
-	echo "Starting Xonotic: \"xon-$2\" \"$2\" \"$3\" \"$4\" \"$5\" \"$_hooked_profile\" \"$_hooked_port\" \"$_hooked_public\" \"$_hooked_type\" \"$_hooked_commands\" \"$_hooked_desc\" \"$_allow_extra_votes\""
+	echo "Starting Xonotic: \"xon-$2\" \"$2\" \"$3\" \"$4\" \"$5\" \"$_hooked_profile\" \"$_hooked_players\" \"$_hooked_port\" \"$_hooked_public\" \"$_hooked_type\" \"$_hooked_commands\" \"$_hooked_desc\" \"$_allow_extra_votes\""
 	screen "$SCREENARGS" xon-"$2" \
 	./all run dedicated \
 	-sessionid "$2" \
 	+set rcon_password \""$3"\" \
 	+set rcon_restricted_password \""$4"\" \
-	"$_hooked_profile" "$_hooked_port" "$_hooked_public" "$_hooked_type" "$_hc_enabled" "$_hooked_commands" "$_hooked_desc" "$_allow_extra_votes" \
+	"$_hooked_profile" "$_hooked_players" "$_hooked_port" "$_hooked_public" "$_hooked_type" "$_hc_enabled" "$_hooked_commands" "$_hooked_desc" "$_allow_extra_votes" \
 	+serverconfig \""$5"\"
 
 	# TODO:
@@ -103,7 +105,7 @@ function _xon-start-wrapper() {
 	# arguments
 	#  ${1}: attached
 	#  ${2}: sessionid
-	#  ${3}: config
+	#  ${3}: hooked_players
 	#  ${4}: hooked_port
 	#  ${5}: hooked_public
 	#  ${6}: hooked_type
@@ -113,31 +115,21 @@ function _xon-start-wrapper() {
 
 	if [ $# -eq 9 ]
 	then
-		_xon-start-explicit "$1" "$2" "$XON_PASS" "$XON_RPASS" "$3" "$XON_IRCENABLED" "$XON_PROFILE" "$4" "$5" "$6" "$7" "$8" "$9"
+		_xon-start-explicit "$1" "$2" "$XON_PASS" "$XON_RPASS" "sv-hookable.cfg" "$XON_IRCENABLED" "$XON_PROFILE" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
 	else
 		echo "_xon-start-wrapper: Incorrect argument count!"
 	fi
 }
 
 # DO NOT CALL THESE DIRECTLY, CALL IT THROUGH THE "xon-start" COMMAND!
-function xon-duel()    { _xon-start-wrapper "$1" "duel"    "sv-hookable.cfg" "27000" "1" "pickup" "duel"             "Duel" "1"; }
-function xon-ctf-mh()  { _xon-start-wrapper "$1" "ctf-mh"  "sv-hookable.cfg" "27000" "1" "public" "ctf; minstahook"  "CTF Instagib+Hook" "0"; }
-function xon-ctf-wa()  { _xon-start-wrapper "$1" "ctf-wa"  "sv-hookable.cfg" "27000" "1" "public" "ctf; weaponarena" "CTF Weaponarena" "0"; }
-function xon-ka-mh()   { _xon-start-wrapper "$1" "ka-mh"   "sv-hookable.cfg" "27000" "1" "public" "ka; minstahook"   "Keepaway Instagib+Hook" "0"; }
-function xon-ka-wa()   { _xon-start-wrapper "$1" "ka-wa"   "sv-hookable.cfg" "27000" "1" "public" "ka; weaponarena"  "Keepaway Weaponarena" "0"; }
-function xon-tourney() { _xon-start-wrapper "$1" "tourney" "sv-hookable.cfg" "27000" "0" "pickup" "duel"             "Tourney" "1"; }
-function xon-votable() { _xon-start-wrapper "$1" "votable" "sv-hookable.cfg" "27000" "1" "public" "dm"               "Votable" "1"; }
-function xon-votable() { _xon-start-wrapper "$1" "lms"     "sv-hookable.cfg" "27000" "1" "public" "lms"              "Last Man Standing" "1"; }
-
-#function xon-duel()    { _xon-start-wrapper "$1" "duel"    "sv-dedicated.cfg" "duel" "echo"        "pickup" "Duel"; }
-#function xon-ctf-mh()  { _xon-start-wrapper "$1" "ctf-mh"  "sv-dedicated.cfg" "ctf"  "minstahook"  "public" "CTF Instagib+Hook"; }
-#function xon-ctf-wa()  { _xon-start-wrapper "$1" "ctf-wa"  "sv-dedicated.cfg" "ctf"  "weaponarena" "public" "CTF Weaponarena"; }
-#function xon-ka-mh()   { _xon-start-wrapper "$1" "ka-mh"   "sv-dedicated.cfg" "ka"   "minstahook"  "public" "Keepaway Instagib+Hook"; }
-#function xon-ka-wa()   { _xon-start-wrapper "$1" "ka-wa"   "sv_dedicated.cfg" "ka"   "weaponarena" "public" "Keepaway Weaponarena"; }
-#function xon-priv-1()  { _xon-start-wrapper "$1" "priv-1"  "sv-private-1.cfg"; }
-#function xon-priv-2()  { _xon-start-wrapper "$1" "priv-2"  "sv-private-2.cfg"; }
-#function xon-tourney() { _xon-start-wrapper "$1" "tourney" "sv-tourney.cfg"; }
-#function xon-votable() { _xon-start-wrapper "$1" "votable" "sv-votable.cfg"; }
+function xon-duel()    { _xon-start-wrapper "$1" "duel"    "16" "27000" "1" "pickup" "duel"             "Duel" "1"; }
+function xon-ctf-mh()  { _xon-start-wrapper "$1" "ctf-mh"  "20" "27000" "1" "public" "ctf; minstahook"  "CTF Instagib+Hook" "0"; }
+function xon-ctf-wa()  { _xon-start-wrapper "$1" "ctf-wa"  "20" "27000" "1" "public" "ctf; weaponarena" "CTF Weaponarena" "0"; }
+function xon-ka-mh()   { _xon-start-wrapper "$1" "ka-mh"   "20" "27000" "1" "public" "ka; minstahook"   "Keepaway Instagib+Hook" "0"; }
+function xon-ka-wa()   { _xon-start-wrapper "$1" "ka-wa"   "20" "27000" "1" "public" "ka; weaponarena"  "Keepaway Weaponarena" "0"; }
+function xon-tourney() { _xon-start-wrapper "$1" "tourney" "32" "27000" "0" "pickup" "duel"             "Tourney" "1"; }
+function xon-votable() { _xon-start-wrapper "$1" "votable" "20" "27000" "1" "public" "dm"               "Votable" "1"; }
+function xon-lms()     { _xon-start-wrapper "$1" "lms"     "20" "27000" "1" "public" "lms"              "Last Man Standing" "1"; }
 
 function _xon-all-bitmissile() {
 	xon-duel "0"
